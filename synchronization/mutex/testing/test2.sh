c6e7ef766709332dd7209a55bec8a880fcbd3fff
Vinoryz <brofistjr10@gmail.com>
2025-12-23 11:59:23 +0700

    fix

--- synchronization/mutex/testing/test2.sh
+++ synchronization/mutex/testing/test2.sh
@@ -3,7 +3,7 @@
 
 # Single PUT to node 1
 python3 ./kvclient.py --nodes 192.168.122.2:8002,192.168.122.3:8003,192.168.122.4:8004,192.168.122.5:8005 cmd --node 1  "PUT color red"
-python3 ./kvclient.py --nodes 192.168.122.2:8002,192.168.122.3:8003,192.168.122.4:8004,192.168.122.5:8005 cmd --node 1  "PUT color blue"
+python3 ./kvclient.py --nodes 192.168.122.2:8002,192.168.122.3:8003,192.168.122.4:8004,192.168.122.5:8005 cmd --node 2  "PUT color blue"
 
 
 # GET from node 2
